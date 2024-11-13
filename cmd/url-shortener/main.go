91af8d8bd2bcee67ddcfbad4ac4cc11a79d3d175
rx3lixir <anxi3ty.exe@proton.me>
2024-11-13 23:45:58 +0700

    added migrations and storage init

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
+	"log/slog"
+
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/rx3lixir/urlshortener/internal/config"
+	"github.com/rx3lixir/urlshortener/internal/lib/logger/sl"
+	"github.com/rx3lixir/urlshortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
 
-	fmt.Println(cfg)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+
+	storage, err := sqlite.New(cfg.Storage)
+	if err != nil {
+		log.Error("Failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,39 @@
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	// For defining error
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	stmt, err := db.Prepare(`
+    CREATE TABLE IF NOT EXISTS url(
+        id INTEGER PRIMARY KEY,
+        alias NEXT NOT NULL UNIQUE,
+        url NEXT NOT NULL);
+    CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
+    `)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
